82ec19ca50151e3afff01aeb443b9964933227a2
Douglas de Jesus <dougecomp@gmail.com>
2023-04-19 00:31:28 -0300

    :sparkles: feat: Adicionando opção de baixar músicas

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 from pytube import YouTube
 
-# Defina o caminho do arquivo que contém os links dos vídeos
-file_path = "urls.txt"
+# Defina o caminho do arquivo que contém os links dos vídeos e músicas
+file_path_mp4 = "urls_mp4.txt"
+file_path_mp3 = "urls_mp3.txt"
 
-# Crie a lista para armazenar os nomes dos vídeos que deram erro
+# Crie a lista para armazenar os nomes dos vídeos e músicas que deram erro
 videos_que_deram_erro = []
+musicas_que_deram_erro = []
 
 # Abra o arquivo em modo de leitura e escrita
-with open(file_path, "r+") as file:
+with open(file_path_mp4, "r+") as file:
     # Leia todas as linhas do arquivo em uma lista
     urls = file.readlines()
 
@@ -35,13 +37,46 @@ with open(file_path, "r+") as file:
             videos_que_deram_erro.append(url)
             print(f"Erro ao baixar o vídeo '{url.strip()}'")
 
-    # Reescreva todo o conteúdo do arquivo com a lista atualizada de links
-    file.seek(0)
+# Atualize os arquivos de URLs com a lista atualizada
+with open(file_path_mp4, "w") as file:
     file.writelines(urls)
-    file.truncate()
+
+# Baixe as músicas em MP3
+with open(file_path_mp3, "r") as file:
+    urls = file.readlines()
+
+    for url in urls:
+        yt = YouTube(url)
+
+        try:
+            # Selecione o áudio em MP3 com a maior qualidade
+            ys = yt.streams.filter(only_audio=True).first()
+
+            # Defina o caminho de saída do arquivo
+            output_path = "./musicas_baixadas/"
+
+            # Faça o download da música
+            ys.download(output_path)
+
+            print(f"Download da música '{yt.title}' completo!")
+            # Se o download for bem-sucedido, remova a linha correspondente do arquivo
+            urls.remove(url)
+        except:
+            # Se a exceção for lançada, adicione o nome da música à lista de erros
+            musicas_que_deram_erro.append(yt.title)
+            print(f"Erro ao baixar a música '{yt.title}'")
+
+# Atualize os arquivos de URLs com a lista atualizada
+with open(file_path_mp3, "w") as file:
+    file.writelines(urls)
 
 # Imprima os nomes dos vídeos que deram erro
 if videos_que_deram_erro:
     print("Vídeos que deram erro:") 
     for video in videos_que_deram_erro:
         print(video)
+
+if musicas_que_deram_erro:
+    print("Músicas que deram erro:") 
+    for musica in musicas_que_deram_erro:
+        print(musica)
